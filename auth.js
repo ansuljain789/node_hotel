5aa0cac781acaabd0b69005b2f9192f3d65a0117
ansul jain <ansuljain789@gmail.com>
2025-01-13 21:14:20 +0530

    password protect

--- auth.js
+++ auth.js
@@ -12,7 +12,8 @@ passport.use(new localStrategy(async(USERNAME,password,done)=>{
    if(!user){
     return done(null,false,{message:'incorrect username.'});
    }
-   const isPasswordMatch = user.password === password ? true : false;
+    // const isPasswordMatch = user.password === password ? true : false;
+    const isPasswordMatch = await user.comparePassword(password);
    if(isPasswordMatch){
     return done(null,user);
    }else{
--- models/Person.js
+++ models/Person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-
+const bcrypt = require('bcrypt');
 //define the peerson schema
 const personSchema = new mongoose.Schema({
     name:{
@@ -42,6 +42,32 @@ const personSchema = new mongoose.Schema({
     }
 
 });
+ personSchema.pre('save',async function(next){
+    const person = this;
+    if( !person.isModified('password')) return next();
+   try{
+
+              ///hash password generate
+              const salt = await bcrypt.genSalt(10);
+              /// hash password
+                 const hashedPassword=  await bcrypt.hashhau(person.password,salt);
+                 //override the plain password with the hashed one 
+                 person.password = hashedPassword;
+              next();
+   }catch(err){
+              return next(err);
+   }
+ })
+
+ personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+         const isMatch = await bcrypt.compare(candidatePassword,this.password);
+         return isMatch;
+
+    }catch(Err){
+        throw err;
+    }
+ }
 
 //create model
 const Person = mongoose.model('Person',personSchema);
